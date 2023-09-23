abf8bf84f32e310b17a659220041f6c1c47d0323
imcm7 <136838917+imcm7@users.noreply.github.com>
2023-09-23 13:48:12 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <VitePwaManifest /> -->
+    <NuxtLoadingIndicator
+      :color="'blue'"
+      :duration="2000"
+      :throttle="200"
+    />
+    <NuxtLayout>
+      <NuxtErrorBoundary @error="errorLogger">
+        <template #error="{ error }">
+          <!-- <button @click="error = null">
+            Clear
+          </button> -->
+        </template>
+      </NuxtErrorBoundary>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+  const errorLogger = (error: any) => {
+    console.log('Error in app.vue', error)
+  }
+</script>
